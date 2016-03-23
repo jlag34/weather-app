c1e5c087ee8b3903880cb44971bfc5855597c160
Joe <joe@servant.co>
2016-03-23 00:33:59 -0400

    GetWeather action runs

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,14 @@
 import axios from 'axios'
 
-
+const API_KEY = '9aaf4ac82210993cb8c8124931f891b0';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
 	const url = `${ROOT_URL}&q=${city},us`;
-	const request = axious.get(url);
+	const request = axios.get(url);
 
 	return {
 		type: FETCH_WEATHER,
