a4d9fec4917dc25b7c320fd455e5525dfaa3755f
Joe <joe@servant.co>
2016-03-22 23:25:17 -0400

    Add first action and promises

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
-const API_KEY = '9aaf4ac82210993cb8c8124931f891b0';
+import axios from 'axios'
+
+
+
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axious.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	};
+}
